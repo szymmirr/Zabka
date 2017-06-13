a4d38c4c843b99addbaa9ff6890995877cafe138
szymmirr <szymmirr@wp.pl>
2017-06-13 19:52:28 +0200

    Add project files.

---
+++ Zabka/Zabka.cpp
@@ -0,0 +1,13 @@
+// Zabka.cpp : Zaplace wam dolara, kupicie sobie za to cala zabke.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	cout << "Zaplace wam dolara, kupicie sobie za to cala zabke" << endl;
+	getchar();
+	getchar();
+    return 0;
+}
